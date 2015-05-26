1c146ef0603bc5af484cd4ccdee462e6b6dffa96
Alex Boklin <alex.boklin@gmail.com>
2015-05-26 17:09:24 +0300

    Добавил тесты для  Marshal/Unmarshal

--- money.go
+++ money.go
@@ -1,6 +1,45 @@
 package money
 
+import (
+	"strconv"
+	"math"
+	"fmt"
+)
+
 //TODO: add doc
+// Хранит деньги как Int, возвращает деленные на 100
 type Money int64
 
+var (
+	roundOn float64 = 0.5
+	places int = 2
+)
+
 //todo: add json/decode/encode
+func (m *Money) UnmarshalJSON(data []byte) error {
+	val, err := strconv.ParseFloat(string(data),64)
+	if err != nil {
+		return err
+	}
+	*m = Money(round(val)*100)
+	return nil
+}
+
+func (m Money) MarshalJSON() ([]byte, error) {
+	return []byte(fmt.Sprintf("%0.2f", float64(m) / 100)), nil
+}
+
+
+func round(val float64) (newVal float64) {
+	var round float64
+	pow := math.Pow(10, float64(places))
+	digit := pow * val
+	_, div := math.Modf(digit)
+	if div >= roundOn {
+		round = math.Ceil(digit)
+	} else {
+		round = math.Floor(digit)
+	}
+	newVal = round / pow
+	return
+}
--- money_test.go
+++ money_test.go
@@ -1,19 +1,50 @@
 //todo: add test with json
 package money
 
-import "testing"
-/*
-[
- {
-  "price": 32.01 // must be 3201
- },
- {
-  "price": 124.01345345 // must be 12401
- },
-] 
-*/
+import (
+	"testing"
+	"encoding/json"
+	"strings"
+	"fmt"
+)
 
-func Test_Encode(t *testing.T) {
-  money := new(Money)
-  print(money)
+func Test_Encode_Decode(t *testing.T) {
+	testPrice := &struct{
+		Price Money `json:"price"`
+	}{}
+
+	tests := []struct {
+		in []byte
+		out []byte
+		storedPrice int64
+	}{
+		{[]byte(`{"price":32.01}`), []byte(`{"price":32.01}`), 3201},
+		{[]byte(`{"price":124.01645345}`), []byte(`{"price":124.02}`), 12402},
+		{[]byte(`{"price":15.22314}`), []byte(`{"price":15.22}`), 1522},
+		{[]byte(`{"price":99.999}`),[]byte(`{"price":100.00}`), 10000},
+		{[]byte(`{"price":99.99}`),[]byte(`{"price":99.99}`), 9999},
+		{[]byte(`{"price":255.654}`),[]byte(`{"price":255.65}`), 25565},
+	}
+
+	for _, test := range tests {
+
+		err1 := json.Unmarshal(test.in, testPrice)
+		if err1 != nil {
+			t.Errorf(fmt.Sprint(err1))
+		}
+		if int64(testPrice.Price) != test.storedPrice {
+			t.Errorf("Extracted incorrect price")
+		}
+
+
+		jsonBytes, err2 := json.Marshal(testPrice)
+		if err2 != nil {
+			t.Errorf(fmt.Sprint(err2))
+		}
+		if !strings.EqualFold(string(jsonBytes), string(test.out)) {
+			t.Errorf("JSON output does not match")
+		}
+
+	}
+
 }
