1c598a882f2a5ad2e736fde1e601dd684bdf8a05
Nikolay Khlopkov <hlopkov@gmail.com>
2015-06-05 12:19:20 +0300

    implemented float and string conversion helpers

--- money.go
+++ money.go
@@ -27,9 +27,24 @@ func (m *Money) UnmarshalJSON(data []byte) error {
 }
 
 func (m Money) MarshalJSON() ([]byte, error) {
-	return []byte(fmt.Sprintf("%0.2f", float64(m) / 100)), nil
+	return []byte(m.String()), nil
 }
 
+func (m Money) String() string {
+	return fmt.Sprintf("%0.2f", m.Float64())
+}
+
+func (m Money) Float64() float64 {
+	return float64(m) / 100
+}
+
+func FromFloat64(v float64) Money {
+	if v >= 0 {
+		return Money(v * 100 + 0.5)
+	} else {
+		return Money(v * 100 - 0.5)
+	}
+}
 
 func round(val float64) (newVal float64) {
 	var round float64
